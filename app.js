ecd9646d505046fa64fc99166423e96a7d8cced7
lamaansaeed <lamaansaeed@gmail.com>
2024-02-12 14:18:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log('in the middleware 1');
+    next();
+});
+app.use((req,res,next)=>{
+    console.log('in the middleware 2');
+    res.send(`<h1> Hello from express!</h1>`);
+});
+
+app.listen(3000);
