bd7bf371d22166a0ea77c9b44b461710a732a51f
lamaansaeed <lamaansaeed@gmail.com>
2024-02-12 21:15:14 +0530

    exporting and importing routes

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use( '/add-product',(req,res,next)=>{
-    res.send(`<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></input></form>`)
-});
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/',(req,res,next)=>{
-    res.send('<h1> Hello From Express<h1>');
+app.use('/admin',adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send(`<h1>Page not Found</h1>`);
 });
 
 app.listen(3000);
