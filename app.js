a552e07be3c5e87df0631623d6360b661c4f90e0
lamaansaeed <lamaansaeed@gmail.com>
2024-02-12 18:37:41 +0530

    adding routes and using body parser

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware 1');
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use( '/add-product',(req,res,next)=>{
+    res.send(`<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></input></form>`)
 });
-app.use((req,res,next)=>{
-    console.log('in the middleware 2');
-    res.send(`<h1> Hello from express!</h1>`);
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/',(req,res,next)=>{
+    res.send('<h1> Hello From Express<h1>');
 });
 
 app.listen(3000);
